7c2822fc0e4fe8827ead710d655b47f9b5e382b1
Ofek-Lutzky <101524820+Ofek-Lutzky@users.noreply.github.com>
2022-08-18 10:55:43 +0300

    Drum Website

---
+++ index.js
@@ -0,0 +1,77 @@
+var allButtons = document.querySelectorAll(".drum")
+var numberOfBtn = allButtons.length
+
+for (var i = 0; i<numberOfBtn;i++){ //this so all the button will respone
+    allButtons[i].addEventListener("click",function() {
+        // alert("button clicked");
+        // this.style.color = "white";
+
+        // var width = this.style.width;
+        // this.style.width = "200px";
+        
+        
+
+        // this.style.width = width;
+        
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+
+document.addEventListener("keypress",function(event){
+    // alert("key press")
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;    
+    
+        default:
+            console.log();
+            break;
+    }
+}
+
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed")
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100)
+}   
